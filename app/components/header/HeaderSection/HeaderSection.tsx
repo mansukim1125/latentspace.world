4926e4cdfa519b054f5e8ec69e041d7635dcd5d9
joonseok kim <mansukim1125@gmail.com>
2025-01-18 20:51:59 +0900

    feat(draft-projects-page): projects 페이지 초안 작성

--- app/components/header/HeaderSection/HeaderSection.tsx
+++ app/components/header/HeaderSection/HeaderSection.tsx
@@ -9,14 +9,16 @@ export const HeaderSection = () => {
           <div className="flex items-center space-x-3">
             <Grid className="w-8 h-8 text-purple-500" />
             <div>
-              <div className="text-lg font-bold tracking-wider">latentspace</div>
-              <div className="text-sm text-purple-500">.world</div>
+              <Link href="/">
+                <div className="text-lg font-bold tracking-wider">latentspace</div>
+                <div className="text-sm text-purple-500">.world</div>
+              </Link>
             </div>
           </div>
           <div className="flex items-center space-x-8">
             <Link href="/writings" className="text-gray-300 hover:text-white">Writing</Link>
             {/*<a href="#" className="text-gray-300 hover:text-white">Writing</a>*/}
-            <a href="#" className="text-gray-300 hover:text-white">Projects</a>
+            <Link href="/projects" className="text-gray-300 hover:text-white">Projects</Link>
             <a href="#" className="text-gray-300 hover:text-white">About</a>
           </div>
         </div>
